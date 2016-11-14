5eb1fbb1a2526497237dc9c9a9c391c1071e599b
chenoajai <winsteadchenoa@gmail.com>
2016-11-14 09:32:34 -0700

    changed file to html

---
+++ lib/partials/featurette.php
@@ -0,0 +1,46 @@
+<!-- START THE FEATURETTES -->
+
+<!--  is goes on the index page-->
+<!-- <?php require_once("lib/partials/featurette.php");?> -->
+
+<hr class="featurette-divider">
+<div class="container">
+  <div class="row featurette">
+      <div class="col-md-7">
+          <h2 class="featurette-heading">Hello.<br> <span class="text-muted">Web Developer and Coder</span></h2>
+          <p class="lead">My name is Chenoa, .</p>
+      </div>
+      <div class="col-md-3 col-md-offset-1">
+          <img class="featurette-image img-responsive center-block" src="lib/images/chenoa1.jpg" alt="thisIsWhereAnImageGoes">
+      </div>
+  </div>
+</div>
+<hr class="featurette-divider">
+<div class="container">
+  <div class="row featurette">
+      <div class="col-md-7 col-md-push-5">
+          <h2 class="featurette-heading"> What do I do? <br> <span class="text-muted">Skills</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
+              semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+      </div>
+      <div class="col-md-5 col-md-pull-7">
+          <img class="featurette-image img-responsive center-block" src="lib/images/sleep1.jpg" alt="thisIsWhereAnImageGoes">
+      </div>
+  </div>
+</div>
+<hr class="featurette-divider">
+<div class="container">
+  <div class="row featurette">
+      <div class="col-md-7">
+          <h2 class="featurette-heading"> Need help? <br> <span class="text-muted"> I'm available for freelance work</span></h2>
+          <p class="lead">If you have a project that you want to get started,
+            think you need my help with something or just fancy saying hey, then get in touch..</p>
+            <button type="button" class="btn btn-primary">Contact Me</button>
+      </div>
+      <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" src="lib/images/xoxo2.jpg" alt="thisIsWhereAnImageGoes">
+      </div>
+  </div>
+</div>
+<hr class="featurette-divider">
+<!-- /END THE FEATURETTES -->
---
+++ lib/partials/polaroid.php
@@ -0,0 +1,29 @@
+<div class="container start">
+  <div class="polaroid">
+    <div class="polaroid-img">
+      <div class="gloss"></div>
+      <img src="http://www.tmarkart.com/codepen/images/scotland-01.jpg" />
+    </div>
+    <p>
+      Kilmuir, Skye
+    </p>
+  </div>
+  <div class="polaroid">
+    <div class="polaroid-img">
+      <div class="gloss"></div>
+      <img src="http://www.tmarkart.com/codepen/images/scotland-03.jpg" />
+    </div>
+    <p>
+      Isle of Skye
+    </p>
+  </div>
+  <div class="polaroid">
+    <div class="polaroid-img">
+      <div class="gloss"></div>
+      <img src="http://www.tmarkart.com/codepen/images/scotland-02.jpg" />
+    </div>
+    <p>
+      Callanish Stones
+    </p>
+  </div>
+</div>
